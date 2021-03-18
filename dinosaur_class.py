cca0a4e5ed908f7f18753aef6a8226cd8872ece9
Rob <rob.sutton123@gmail.com>
2021-03-18 15:03:41 -0400

    robot class and dinosaur class have been given their attributes and I still need to create different functions for each of the attributes given to them

---
+++ dinosaur_class.py
@@ -0,0 +1,9 @@
+
+
+class Dinosaur:
+    def __init__(self):
+        self.type = ''
+        self.health = ''
+        self.energy = ''
+        self.attack_power = ''
+
---
+++ robot_class.py
@@ -0,0 +1,9 @@
+
+
+class Robot:
+    def __init__(self):
+        self.name = ''
+        self.health = ''
+        self.power_level = ''
+        self.weapon = ''
+        self.attack_power = ''
